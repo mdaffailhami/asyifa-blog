03a51db2ed3b3c1f1b6676f2598b7bc2bc3571f6
mdaffailhami <mdaffailhami@gmail.com>
2021-11-23 12:34:57 +0800

    Kelar storage with firestore sisa search lagi yg balum

--- src/admin/storage/script.js
+++ src/admin/storage/script.js
@@ -1,9 +1,26 @@
 import { onAuthStateChanged } from "@firebase/auth";
-import { getDownloadURL, getMetadata, list, ref, uploadBytesResumable } from "@firebase/storage";
+import {
+  getDownloadURL,
+  getMetadata,
+  list,
+  ref,
+  uploadBytesResumable,
+  deleteObject,
+} from "@firebase/storage";
+import {
+  setDoc,
+  doc,
+  getDocs,
+  query,
+  collection,
+  orderBy,
+  limit,
+  startAfter,
+} from "@firebase/firestore";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import { alert, footer, loader, navbar } from "../../../elements";
-import { auth, storage } from "../../../modules/firebase";
+import { auth, firestore, storage } from "../../../modules/firebase";
 
 // Check user
 onAuthStateChanged(auth, async (user) => {
@@ -23,22 +40,63 @@ onAuthStateChanged(auth, async (user) => {
   const fileInput = document.getElementById("file-input");
   const uploadProgess = document.getElementById("upload-progress");
 
-  // add loader to files container
+  // loader
   const filesContainerLoader = loader();
-  filesContainer.insertAdjacentElement("afterbegin", filesContainerLoader);
+  filesContainer.insertAdjacentElement("beforeend", filesContainerLoader);
 
-  try {
-    // Get files
-    const files = await list(ref(storage), { maxResults: 10 });
+  // Limit files
+  const limitFiles = 10;
 
-    let index = 1;
-    files.items.forEach(async (file) => {
-      const fileUrl = await getDownloadURL(ref(storage, file.fullPath));
-      const fileMetaData = await getMetadata(ref(storage, file.fullPath));
+  // latest File
+  let latestFile = null;
 
-      filesContainer.insertAdjacentHTML(
-        "beforeend",
-        `
+  // loading Files condition
+  let isLoadingFiles = false;
+
+  // load first files
+  getFiles(
+    query(collection(firestore, "files"), orderBy("timeCreated", "desc"), limit(limitFiles))
+  );
+
+  // load more articles on scrolled to bottom
+  filesContainer.addEventListener("scroll", onFilesContainerScrolledToBottom);
+
+  function onFilesContainerScrolledToBottom() {
+    const triggerHeight = filesContainer.scrollTop + filesContainer.offsetHeight;
+    if (triggerHeight >= filesContainer.scrollHeight) {
+      getFiles(
+        query(
+          collection(firestore, "files"),
+          orderBy("timeCreated", "desc"),
+          startAfter(latestFile),
+          limit(limitFiles)
+        )
+      );
+    }
+  }
+
+  async function getFiles(query) {
+    try {
+      if (isLoadingFiles) return;
+      isLoadingFiles = true;
+      // add loader to files container
+      filesContainer.insertAdjacentElement("beforeend", filesContainerLoader);
+
+      // Get files
+      const files = await getDocs(query);
+
+      if (files.empty) {
+        filesContainer.removeEventListener("scroll", onFilesContainerScrolledToBottom);
+        return;
+      }
+
+      let index = 1;
+      files.forEach(async (file) => {
+        file = file.data();
+
+        filesContainer.insertAdjacentHTML(
+          "beforeend",
+          `
           <div id="item-${index}" class="accordion-item">
             <h2 class="accordion-header" id="heading-${index}">
             <button
@@ -49,7 +107,7 @@ onAuthStateChanged(auth, async (user) => {
               aria-expanded="true"
               aria-controls="collapse-${index}"
             >
-              ${fileMetaData.name}
+              ${file.name}
             </button>
           </h2>
           <div
@@ -68,7 +126,7 @@ onAuthStateChanged(auth, async (user) => {
                         style="
                           width: 200px;
                           height: 200px;
-                          background-image: url(${fileUrl});
+                          background-image: url(${file.url});
                           overflow: hidden;
                           background-position: center;
                           background-size: contain;
@@ -76,7 +134,7 @@ onAuthStateChanged(auth, async (user) => {
                         "
                       >
                         <img
-                          src="${fileUrl}"
+                          src="${file.url}"
                           style="opacity: 0"
                         />
                       </div>
@@ -85,20 +143,32 @@ onAuthStateChanged(auth, async (user) => {
                   <li class="list-group-item d-flex justify-content-between align-items-start">
                     <div class="ms-2 me-auto">
                       <div class="fw-bold">Name</div>
-                      <a href="${fileUrl}" target="_blank">${fileMetaData.name}</a>
+                      <a href="${file.url}" target="_blank">${file.name}</a>
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                      <div class="fw-bold">Media Type</div>
+                      ${file.mediaType}
+                    </div>
+                  </li>
+                  <li class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                      <div class="fw-bold">Size</div>
+                      ${Number(file.size / 1024 / 1024).toFixed(2)} MB
                     </div>
                   </li>
                   <li class="list-group-item d-flex justify-content-between align-items-start">
                     <div class="ms-2 me-auto">
                       <div class="fw-bold">Uploaded</div>
-                      ${new Date(fileMetaData.timeCreated).toLocaleDateString()}
+                      ${new Date(file.timeCreated).toLocaleDateString()}
                     </div>
                   </li>
                   <li class="list-group-item d-flex justify-content-between align-items-start">
                     <div class="ms-2 me-auto" style="word-break: break-all">
                       <div class="fw-bold">URL</div>
                       <div>
-                        ${fileUrl}
+                        ${file.url}
                       </div>
                       <button
                         class="btn btn-primary"
@@ -124,9 +194,9 @@ onAuthStateChanged(auth, async (user) => {
                                   textArea.remove();
                               });
                           }
-                        })('${fileUrl}')
-                          .then(() => window.alert('Text copied!'))
-                          .catch(() => window.alert('Failed to copy text!'));
+                        })('${file.url}')
+                          .then(() => window.alert('URL copied!'))
+                          .catch(() => window.alert('Failed to copy URL!'));
                         "
                       >
                         Copy URL
@@ -139,18 +209,22 @@ onAuthStateChanged(auth, async (user) => {
           </div>
         </div>
       `
-      );
+        );
 
-      // Increment index
-      index++;
-    });
-  } catch (error) {
-    console.error(error);
-    // Error alert
-    filesContainer.insertAdjacentElement("afterbegin", alert("danger", "Failed to get files!"));
-  } finally {
-    // Remove loader
-    filesContainer.removeChild(filesContainerLoader);
+        // Increment index
+        index++;
+        // increment latest files
+        latestFile = files.docs[files.docs.length - 1];
+      });
+    } catch (error) {
+      console.error(error);
+      // Error alert
+      filesContainer.insertAdjacentElement("afterbegin", alert("danger", "Failed to get files!"));
+    } finally {
+      // Remove loader
+      filesContainer.removeChild(filesContainerLoader);
+      isLoadingFiles = false;
+    }
   }
 
   // Upload File handler
@@ -176,10 +250,30 @@ onAuthStateChanged(auth, async (user) => {
         window.alert("Upload file failed!");
         uploadProgess.style.display = "none";
       },
-      () => {
-        window.alert("File uploaded!");
-        // Refresh page
-        window.location.reload();
+      async () => {
+        // Jika berhasil mengupload file ke Cloud Storage Firebase
+        try {
+          const fileMetaData = await getMetadata(ref(storage, fileName));
+          const fileUrl = await getDownloadURL(ref(storage, fileName));
+
+          await setDoc(doc(firestore, "files", fileName), {
+            name: fileName,
+            mediaType: fileMetaData.contentType,
+            size: fileMetaData.size,
+            timeCreated: fileMetaData.timeCreated,
+            url: fileUrl,
+          });
+
+          window.alert("File uploaded!");
+          // Refresh page
+          window.location.reload();
+        } catch (error) {
+          console.error(error);
+          window.alert("Upload file failed!");
+          uploadProgess.style.display = "none";
+
+          deleteObject(ref(storage, fileName));
+        }
       }
     );
   });
